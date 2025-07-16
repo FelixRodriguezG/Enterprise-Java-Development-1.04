c623d078707ac4ffa5c919ebf05309532b579b65
FelixRodriguezG <correofrg@gmail.com>
2025-07-16 14:39:51 +0200

    add a try-catch block to handle errors

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,9 +9,12 @@ public class Main {
         System.out.println("Task 1 -> Finding the difference between the largest and smallest values in the array.");
 
         // Call the method and print the result
-        int difference = MathOperations.findDifference(numbers);
-        System.out.println("    - The difference between the largest and smallest values is: " + difference);
-
+        try {
+            int difference = MathOperations.findDifference(numbers);
+            System.out.println("    - The difference between the largest and smallest values is: " + difference);
+        } catch (IllegalArgumentException error) {
+            System.err.println("    - Error: "  + error.getMessage());
+        }
         /*Write a Java method that finds the smallest and second smallest elements of a given array and prints them to the console.
         Task 2*/
         // Create an instance of Utils to access the method
@@ -19,14 +22,21 @@ public class Main {
         // Define an array of integers
         int[] array = {7, 2, 5, 1, 3};
         // Call the method to find and print the two smallest numbers
-        System.out.println("Task 2 -> Finding the two smallest numbers in the array.");
-        utilities.findTwoSmallestNumbers(array);
+        try{
+            System.out.println("Task 2 -> Finding the two smallest numbers in the array.");
+            // Call the method to find and print the two smallest numbers
+            utilities.findTwoSmallestNumbers(array);
+        } catch (IllegalArgumentException error) {
+            System.err.println("    - Error: " + error.getMessage());
+        }
 
         /*Write a Java method that calculates the result of the following mathematical
         expression, where x and y are two variables that have been pre-set in your code*/
         // Task 3
-        var x = 3;
-        var y = 4;
+        // Define the variables x and y
+        var x = 3.44;
+        var y = 4.67;
+        // Call the method to calculate the result of the expression
         double result = MathOperations.calculateExpression(x, y);
         System.out.println("Task 3 -> Calculating the result of the mathematical expression.");
         System.out.println("    - The result of the expression is: " + result);
